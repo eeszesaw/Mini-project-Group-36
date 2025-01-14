ff0f485209dd66272412d4346909738e6def7605
junxiang2 <chongjunxiang2@student.usm.my>
2025-01-14 19:13:59 +0800

    Contact.cpp

--- Contact.cpp
+++ Contact.cpp
@@ -1,45 +1,34 @@
-##include "Contact.hpp"
+#include "Contact.hpp"
+#include <iostream>
 
-// Constructor
-Contact::Contact(const std::string& firstName, const std::string& lastName,
-                 const std::string& phone, const std::string& email)
-    : firstName(firstName), lastName(lastName), phone(phone), email(email) {}
+using namespace std;
 
-// Getters
-std::string Contact::getFullName() const {
-    return firstName + " " + lastName;
+
+Contact::Contact(const string& firstName, const string& lastName, const string& phone, const string& email,
+                 const string& birthday, const string& note)
+    : firstName(firstName), lastName(lastName), phone(phone), email(email), birthday(birthday), note(note) {}
+
+
+string Contact::getFirstName() const { return firstName; }
+string Contact::getLastName() const { return lastName; }
+string Contact::getPhone() const { return phone; }
+string Contact::getEmail() const { return email; }
+string Contact::getBirthday() const { return birthday; }
+string Contact::getNote() const { return note; }
+
+
+void Contact::setFirstName(const string& firstName) { this->firstName = firstName; }
+void Contact::setLastName(const string& lastName) { this->lastName = lastName; }
+void Contact::setPhone(const string& phone) { this->phone = phone; }
+void Contact::setEmail(const string& email) { this->email = email; }
+void Contact::setBirthday(const string& birthday) { this->birthday = birthday; }
+void Contact::setNote(const string& note) { this->note = note; }
+
+
+void Contact::display() const {
+    cout << "Name: " << firstName << " " << lastName << endl;
+    cout << "Phone: " << phone << endl;
+    cout << "Email: " << email << endl;
+    cout << "Birthday: " << birthday << endl;
+    cout << "Note: " << note << endl;
 }
-
-std::string Contact::getFirstName() const {
-    return firstName;
-}
-
-std::string Contact::getLastName() const {
-    return lastName;
-}
-
-std::string Contact::getPhone() const {
-    return phone;
-}
-
-std::string Contact::getEmail() const {
-    return email;
-}
-
-// Setters
-void Contact::setFirstName(const std::string& firstName) {
-    this->firstName = firstName;
-}
-
-void Contact::setLastName(const std::string& lastName) {
-    this->lastName = lastName;
-}
-
-void Contact::setPhone(const std::string& phone) {
-    this->phone = phone;
-}
-
-void Contact::setEmail(const std::string& email) {
-    this->email = email;
-}
-
