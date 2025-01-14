081d6b85b168e11b787708593d3f07ee2aa5ea68
junxiang2 <chongjunxiang2@student.usm.my>
2025-01-14 19:19:11 +0800

    EditContact.hpp

--- EditContact.hpp
+++ EditContact.hpp
@@ -1,25 +1,6 @@
 #ifndef EDITCONTACT_HPP
 #define EDITCONTACT_HPP
 
-#include <iostream>
-#include <string>
-#include <unordered_map>
-
-class Contact {
-public:
-    std::string name;
-    std::string phoneNumber;
-    std::string email;
-
-    Contact(std::string n, std::string p, std::string e);
-};
-
-class ContactDirectory {
-private:
-    std::unordered_map<std::string, Contact> contacts;
-
-public:
-    void editContact(const std::string& name);
-};
+void editContact();
 
 #endif 
