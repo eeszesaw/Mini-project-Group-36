6e6b2c871791b1c6845550b3e908afbe9bff2404
junxiang2 <chongjunxiang2@student.usm.my>
2025-01-19 21:56:12 +0800

    ContactManager.hpp

--- ContactManager.hpp
+++ ContactManager.hpp
@@ -13,11 +13,12 @@ public:
     void deleteContact(const std::string& fullName);
     bool loadContacts(const std::string& filename);
     bool saveContacts(const Contact& newContact, const std::string& filename);
+    void saveAllContacts();  // Save all contacts to file
 
 private:
     std::vector<Contact> contacts;  // Stores all contacts in memory
     const std::string filename = "contacts.txt";
-    void saveAllContacts();  // Save all contacts to file
+    
 };
 
 #endif 
